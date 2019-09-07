60bdc9909f1b28783f654107367066e52ca68fed
Johan Todi <johantodi@huntsman-ve702-4877.apng.wlan.private.upenn.edu>
2019-09-07 01:27:17 -0400

    initial setup

---
+++ shopAR/shopAR.h
@@ -0,0 +1,19 @@
+//
+//  shopAR.h
+//  shopAR
+//
+//  Created by Johan Todi on 2019-09-07.
+//  Copyright © 2019 GFE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for shopAR.
+FOUNDATION_EXPORT double shopARVersionNumber;
+
+//! Project version string for shopAR.
+FOUNDATION_EXPORT const unsigned char shopARVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <shopAR/PublicHeader.h>
+
+
---
+++ shopARExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  shopARExamples
+//
+//  Created by Johan Todi on 2019-09-07.
+//  Copyright © 2019 GFE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
